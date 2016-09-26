30722185593b969358ea1645af68dc14424c4009
edvampalma6 <ed.psilveira6@gmail.com>
2016-09-26 16:56:13 -0300

    Services add

---
+++ services/bmi.action.php
@@ -0,0 +1,11 @@
+<?php
+require 'BMIService.class.php';
+$weight = $_GET['weight'];
+$height = $_GET['height'];
+$bmiIndex = 0.0;
+if(is_numeric($weight) && is_numeric($height)) {
+    $bmiIndex = BMIService::getIndex($weight, $height);
+}
+sleep (1);
+echo $bmiIndex;
+?>
